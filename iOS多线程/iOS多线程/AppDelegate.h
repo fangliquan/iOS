cb64e006487acc9684f5c88fe22ef14067381665
leo <976436569@qq.com>
2016-07-28 14:54:25 +0800

    多线程学习

---
+++ iOS多线程/iOS多线程/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS多线程
+//
+//  Created by leo on 16/7/28.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS多线程/iOS多线程/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS多线程
+//
+//  Created by leo on 16/7/28.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
