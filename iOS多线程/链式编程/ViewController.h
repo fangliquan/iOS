9e0421172bfc19698c815e3c71124eba12a8af5d
leo <976436569@qq.com>
2016-08-04 09:16:31 +0800

    链式编程

---
+++ iOS多线程/链式编程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  链式编程
+//
+//  Created by leo on 16/8/3.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS多线程/链式编程/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  链式编程
+//
+//  Created by leo on 16/8/3.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
