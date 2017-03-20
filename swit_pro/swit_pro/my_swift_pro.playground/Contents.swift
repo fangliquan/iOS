3956621aa3ae02ce6e072b1eeeb966ce1cbdf078
leo <976436569@qq.com>
2017-03-20 17:59:33 +0800

    swift

--- swit_pro/swit_pro/my_swift_pro.playground/Contents.swift
+++ swit_pro/swit_pro/my_swift_pro.playground/Contents.swift
@@ -3,3 +3,47 @@
 import UIKit
 
 var str = "Hello, playground"
+
+protocol ExampleProtocal{
+    var simpleDesp:String{get}
+    mutating func adjust()
+    
+}
+class SimpleClass: ExampleProtocal{
+    var simpleDesp: String = "A simple class"
+    var anotherProperty:Int = 45621
+    func adjust() {
+        simpleDesp += " NO 100% adjusted"
+    }
+    
+}
+
+var simple = SimpleClass()
+simple.adjust()
+
+let aDesp = simple.simpleDesp
+
+struct SimpleStructure:ExampleProtocal{
+    var simpleDesp: String = "A simple structure"
+    mutating func adjust() {
+        simpleDesp += "(adjusted)"
+    }
+    
+}
+var b = SimpleStructure()
+b.adjust()
+let bDesp = b.simpleDesp
+
+extension Int:ExampleProtocal {
+    var simpleDesp :String {
+        return "The number\(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+    
+}
+
+print(7.simpleDesp)
+
+		
